8714788484327da2fdcff92e645390f703f49231
saurabh3j <saurabh3j@gmail.com>
2026-01-01 21:59:28 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,22 +1,63 @@
-import{db,auth}from'./firebase.js';
-import{collection,addDoc,getDocs,deleteDoc,doc}from'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
-const blogRef=collection(db,'blogs');
-const msgRef=collection(db,'contacts');
+import { db, auth } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-window.addBlog=async()=>{
-await addDoc(blogRef,{title:title.value,category:category.value,content:content.value,author:auth.currentUser.email,createdAt:new Date()});
-title.value=category.value=content.value='';
-load();
+import {
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const blogRef = collection(db, "blogs");
+const msgRef = collection(db, "contacts");
+
+const blogs = document.getElementById("blogs");
+const messages = document.getElementById("messages");
+const logoutBtn = document.getElementById("logoutBtn");
+
+// ADD BLOG
+window.addBlog = async () => {
+  await addDoc(blogRef, {
+    title: title.value,
+    category: category.value,
+    content: content.value,
+    author: auth.currentUser.email,
+    published: true,
+    createdAt: new Date()
+  });
+
+  title.value = "";
+  category.value = "";
+  content.value = "";
+
+  load();
 };
 
-async function load(){
-blogs.innerHTML='';
-(const await getDocs(blogRef)).forEach(d=>{
-blogs.innerHTML+=`<p><b>${d.data().title}</b></p>`;
-});
-messages.innerHTML='';
-(const await getDocs(msgRef)).forEach(d=>{
-messages.innerHTML+=`<p><b>${d.data().email}</b>: ${d.data().message}</p>`;
-});
+// LOAD BLOGS & MESSAGES
+async function load() {
+  blogs.innerHTML = "";
+  messages.innerHTML = "";
+
+  const blogSnap = await getDocs(blogRef);
+  blogSnap.forEach(d => {
+    blogs.innerHTML += `<p><b>${d.data().title}</b></p>`;
+  });
+
+  const msgSnap = await getDocs(msgRef);
+  msgSnap.forEach(d => {
+    messages.innerHTML += `<p><b>${d.data().email}</b>: ${d.data().message}</p>`;
+  });
 }
+
+// LOGOUT
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "login.html";
+  });
+}
+
 load();
