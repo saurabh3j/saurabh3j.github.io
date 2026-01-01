2ed6721271fdbb3686e5435dfd590049d86304b2
saurabh3j <saurabh3j@gmail.com>
2026-01-01 19:11:12 +0530

    Delete auth.js

--- auth.js
+++
@@ -1,9 +0,0 @@
-import{auth}from'./firebase.js';
-import{signInWithEmailAndPassword,signOut}from'https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js';
-const admins=['admin@safenex.co.in','saurabhs.yedekar@gmail.com'];
-window.login=()=>{
-signInWithEmailAndPassword(auth,email.value,password.value)
-.then(u=>{admins.includes(u.user.email)?location.href='dashboard.html':signOut(auth)})
-.catch(e=>error.innerText=e.message)
-}
-window.logout=()=>signOut(auth).then(()=>location.href='login.html');
