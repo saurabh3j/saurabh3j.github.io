cec18023f993f2ecb8ead24cd9e138e0a48bc33b
saurabh3j <saurabh3j@gmail.com>
2026-01-01 20:17:25 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBZjmaAQkX4qzWt3JBVzVtPbVtGPbb7EXI",
+  apiKey: "YOUR_API_KEY",
   authDomain: "safenex-33a34.firebaseapp.com",
   projectId: "safenex-33a34",
-  storageBucket: "safenex-33a34.firebasestorage.app",
+  storageBucket: "safenex-33a34.appspot.com",
   messagingSenderId: "721188802838",
-  appId: "1:721188802838:web:f75bb9081d12ccb667c037",
-  measurementId: "G-7SDMCY7942"
+  appId: "YOUR_APP_ID"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
