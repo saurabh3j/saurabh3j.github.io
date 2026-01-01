99b285a742c8abaa81ad31e4b3cd5fc46ef4aa21
saurabh3j <saurabh3j@gmail.com>
2026-01-01 19:12:04 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-import{db}from'./firebase.js';
-import{addDoc,collection}from'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
-window.send=async()=>{
-await addDoc(collection(db,'contacts'),{name:name.value,email:email.value,message:message.value,createdAt:new Date()});
-alert('Message sent successfully');
-name.value=email.value=message.value='';
-};
