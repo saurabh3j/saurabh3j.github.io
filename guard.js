9151f981111dc01664a3ffa69be417ed696028db
saurabh3j <saurabh3j@gmail.com>
2026-01-01 19:11:53 +0530

    Delete guard.js

--- guard.js
+++
@@ -1,4 +0,0 @@
-import{auth}from'./firebase.js';
-import{onAuthStateChanged}from'https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js';
-const admins=['admin@safenex.co.in','saurabhs.yedekar@gmail.com'];
-onAuthStateChanged(auth,u=>{if(!u||!admins.includes(u.email))location.href='login.html';});
